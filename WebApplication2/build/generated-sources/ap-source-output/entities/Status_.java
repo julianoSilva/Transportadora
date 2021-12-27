bd377a4fcc1b5325527ce6bbfba0c64d2f36ccac
julianoSilva <julianosgr@hotmail.com>
2021-12-27 10:22:52 -0300

    Add files via upload

---
+++ WebApplication2/build/generated-sources/ap-source-output/entities/Status_.java
@@ -0,0 +1,15 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2021-12-27T09:20:23")
+@StaticMetamodel(Status.class)
+public class Status_ { 
+
+    public static volatile SingularAttribute<Status, Long> ident;
+    public static volatile SingularAttribute<Status, Long> id;
+    public static volatile SingularAttribute<Status, String> message;
+
+}
